d6fa60a9709ec98ed26a0239c4b9c26ea60ed0a0
ahazimeh <hazimeh95@gmail.com>
2020-10-16 13:34:18 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -6,3 +6,16 @@ Basic Queries
 5-INSERT INTO students(name,Age,Gender,Points)VALUES('Ali',26,'M',300)
 6-UPDATE students SET	Points = 30+(SELECT Points from students WHERE name like 'Basma') WHERE name like 'Basma'
 7-UPDATE students SET	Points = (SELECT Points from students WHERE name like 'Alex')-10 WHERE name like 'Alex'
+Creating Table
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	TEXT,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID" AUTOINCREMENT)
+);
+INSERT INTO graduates(name,Age,Gender,Points) select name,Age,Gender,Points FROM students WHERE name like 'layal'
+UPDATE graduates SET Graduation = '08/09/2018' WHERE name like 'Layal'
+DELETE FROM students WHERE name like 'Layal'
